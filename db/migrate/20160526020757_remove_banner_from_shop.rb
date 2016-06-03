534d329efa06ade4955d909efcb47bd885bb2620
Nana Adjei Manu <claeusdev@gmail.com>
2016-06-03 00:42:04 +0000

    Removed shop banner

--- db/migrate/20160526020757_remove_banner_from_shop.rb
+++
@@ -1,8 +0,0 @@
-class RemoveBannerFromShop < ActiveRecord::Migration
-  def change
-    remove_column :shops, :banner_file_name, :string
-    remove_column :shops, :banner_content_type, :string
-    remove_column :shops, :banner_file_size, :integer
-    remove_column :shops, :banner_updated_at, :datetime
-  end
-end
